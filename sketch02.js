dcb7acc90d5c5bcd181582789c4f54f4c804f23e
Alwin Nedrum <nedrum@gmail.com>
2021-12-27 19:30:38 +0100

    update

--- sketch02.js
+++ sketch02.js
@@ -1,8 +1,16 @@
 const canvasSketch = require("canvas-sketch");
+const math = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
 
 const settings = {
   dimensions: [1080, 1080],
 };
+
+const pi = Math.PI;
+// const degToRad = (degrees) => (degrees / 180) * pi;
+// const randomRange = (min, max) => {
+//   return Math.random() * (max - min) + min;
+// };
 
 const sketch = () => {
   return ({ context, width, height }) => {
@@ -10,18 +18,47 @@ const sketch = () => {
     context.fillRect(0, 0, width, height);
 
     context.fillStyle = "black";
-    console.log(width);
-    const x = width * 0.5;
-    const y = height * 0.5;
-    const w = width * 0.3;
-    const h = height * 0.3;
+    console.log(width * 0.5);
+    const cx = width * 0.5;
+    const cy = height * 0.5;
+    const w = width * 0.01;
+    const h = height * 0.1;
+    let x, y;
 
-    context.translate(x, y);
-    context.rotate(0);
+    const num = 12;
+    const radius = width * 0.3;
 
-    context.beginPath();
-    context.rect(w * 0.5, h * 0.5, w, h);
-    context.fill();
+    for (let i = 0; i < num; i++) {
+      const slice = math.degToRad(360 / num);
+      const angle = slice * i;
+
+      x = cx + radius * Math.sin(angle);
+      y = cy + radius * Math.cos(angle);
+
+      context.save();
+      context.translate(x, y);
+      context.rotate(-angle);
+      context.scale(random.range(3, 1), 1);
+      context.beginPath();
+      context.rect(-w * 0.5, -h * 0.5, w, h);
+      context.fill();
+      context.restore();
+
+      context.save();
+      context.translate(cx, cy);
+      context.rotate(angle);
+      context.lineWidth = 20;
+      context.beginPath();
+      context.arc(0, 0, radius, slice * -0.3, slice * 0.3);
+      context.stroke();
+      context.restore();
+    }
+
+    // context.translate(100, 400);
+
+    // context.beginPath();
+    // context.arc(0, 0, 50, 0, pi * 2);
+    // context.fill();
   };
 };
 
